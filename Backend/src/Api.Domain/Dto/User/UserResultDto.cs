1e1bf7111817389b6037c9b07ba00d3f48334765
RayanCardoso <rayancardoso40@gmail.com>
2025-04-26 09:29:14 -0300

    feito a mudança de arquitetura de pastas

---
+++ Backend/src/Api.Domain/Dto/User/UserResultDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain.Dto.Setor;
+
+namespace Api.Domain.Dto
+{
+    public class UserResultDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
