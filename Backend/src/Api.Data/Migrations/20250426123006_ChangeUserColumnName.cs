ca7a0d55c0dcbad1a0a0912217beab199aa4c4b0
RayanCardoso <rayancardoso40@gmail.com>
2025-04-26 09:30:42 -0300

    update database ajustes finos

---
+++ Backend/src/Api.Data/Migrations/20250426123006_ChangeUserColumnName.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class ChangeUserColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "User",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
